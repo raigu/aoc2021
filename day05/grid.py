dad0cd8c1d1d471945eeffec67aac135f5429382
Rait Kapp <3442567+raigu@users.noreply.github.com>
2021-12-05 09:32:18 +0200

    Move logic to grid

--- day05/grid.py
+++ day05/grid.py
@@ -1,4 +1,12 @@
+from enum import Enum
+
 from day05.point import Point
+
+
+class Direction(Enum):
+    HORIZONTAL = 0
+    VERTICAL = 1
+    DIAGONAL = 2
 
 
 class Grid:
@@ -17,3 +25,28 @@ class Grid:
 
     def points(self) -> list[Point]:
         return list(self._grid.keys())
+
+    def draw_lines(self, lines, directions: set[Direction]) -> None:
+        for b, e in lines:
+            if Direction.HORIZONTAL in directions and b[0] == e[0]:
+                for y in range(min(b[1], e[1]), max(b[1], e[1]) + 1):
+                    self.add(Point(b[0], y))
+
+            if Direction.VERTICAL in directions and b[1] == e[1]:
+                for x in range(min(b[0], e[0]), max(b[0], e[0]) + 1):
+                    self.add(Point(x, b[1]))
+
+            if Direction.DIAGONAL in directions and abs(b[0] - e[0]) == abs(b[1] - e[1]):
+                r = range(0, abs(b[0] - e[0]) + 1)
+                xsign = 1 if b[0] < e[0] else -1
+                ysign = 1 if b[1] < e[1] else -1
+
+                for i in r:
+                    self.add(Point(b[0] + xsign * i, b[1] + ysign * i))
+
+    def number_of_intersecting_points(self) -> int:
+        answer = 0
+        for point in self.points():
+            if self._grid[point] > 1:
+                answer += 1
+        return answer
--- day05/main.py
+++ day05/main.py
@@ -1,43 +1,5 @@
-from enum import Enum
-
-from day05.grid import Grid
+from day05.grid import Grid, Direction
 from day05.point import Point
-
-
-class Direction(Enum):
-    HORIZONTAL = 0
-    VERTICAL = 1
-    DIAGONAL = 2
-
-
-def draw_lines(line_of_vents, directions: set[Direction]) -> Grid:
-    grid = Grid()
-    for b, e in line_of_vents:
-        if Direction.HORIZONTAL in directions and b[0] == e[0]:
-            for y in range(min(b[1], e[1]), max(b[1], e[1]) + 1):
-                grid.add(Point(b[0], y))
-
-        if Direction.VERTICAL in directions and b[1] == e[1]:
-            for x in range(min(b[0], e[0]), max(b[0], e[0]) + 1):
-                grid.add(Point(x, b[1]))
-
-        if Direction.DIAGONAL in directions and abs(b[0] - e[0]) == abs(b[1] - e[1]):
-            r = range(0, abs(b[0] - e[0]) + 1)
-            xsign = 1 if b[0] < e[0] else -1
-            ysign = 1 if b[1] < e[1] else -1
-
-            for i in r:
-                grid.add(Point(b[0] + xsign * i, b[1] + ysign * i))
-
-    return grid
-
-
-def number_of_intersecting_points(grid: Grid) -> int:
-    answer = 0
-    for point in grid.points():
-        if grid[point] > 1:
-            answer += 1
-    return answer
 
 
 if __name__ == '__main__':
@@ -52,8 +14,10 @@ if __name__ == '__main__':
 
         lines_of_vents.append([b, e])
 
-    grid = draw_lines(lines_of_vents, {Direction.HORIZONTAL, Direction.VERTICAL})
-    print('Part 1: ', number_of_intersecting_points(grid))
+    part1 = Grid()
+    part1.draw_lines(lines_of_vents, {Direction.HORIZONTAL, Direction.VERTICAL})
+    print('Part 1: ', part1.number_of_intersecting_points())
 
-    grid = draw_lines(lines_of_vents, {Direction.HORIZONTAL, Direction.VERTICAL, Direction.DIAGONAL})
-    print('Part 2: ', number_of_intersecting_points(grid))
+    part2 = Grid()
+    part2.draw_lines(lines_of_vents, {Direction.HORIZONTAL, Direction.VERTICAL, Direction.DIAGONAL})
+    print('Part 2: ', part2.number_of_intersecting_points())
