241a3e3ef21e2ae8591582a6303318f0c3493cd3
Rait Kapp <3442567+raigu@users.noreply.github.com>
2021-12-06 07:16:52 +0200

    Day 06, initial part1

--- day06/main.py
+++ day06/main.py
@@ -1,9 +1,27 @@
-def part1(data):
+def part1(data,days):
+    """
+    >>> part1([3,4,3,1,2], 18)
+    26
+    >>> part1([3,4,3,1,2],80)
+    5934
+    >>> part1([3,4,3,1,2],256)
+    26984457539
+    """
     answer = 0
 
-    # Solution here
+    new = list(data)
+    for day in range(days):
+        fishes = list(new)
+        new = []
+        for fish in list(fishes):
+            if fish == 0:
+                fish = 6
+                new.append(8)
+            else:
+                fish = fish - 1
+            new.append(fish)
 
-    return answer
+    return len(new)
 
 
 def part2(data):
@@ -16,6 +34,7 @@ def part2(data):
 
 if __name__ == '__main__':
     with open('input') as f:
-        lines = f.readlines()
-    print('Part 1: ', part1(lines))
-    print('Part 2: ', part2(lines))
+        line = f.readline().strip()
+
+    print('Part 1: ', part1(map(int, line.split(',')), 80))
+#    print('Part 2: ', part2(lines))
