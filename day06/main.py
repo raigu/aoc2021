b39b540c82f89c4f148d3be5a049f5e710c8b541
Rait Kapp <3442567+raigu@users.noreply.github.com>
2021-12-06 09:45:04 +0200

    Day 06, part2 final

--- day06/main.py
+++ day06/main.py
@@ -77,6 +77,61 @@ def part2(fishes, days):
     return answer
 
 
+def part3(data, days):
+    """
+    >>> part3([3,4,3,1,2], 3)
+    7
+    >>> part3([0], 1)
+    2
+    >>> part3([3,4,3,1,2], 1)
+    5
+    >>> part3([0], 7)
+    2
+    >>> part3([0], 6)
+    2
+    >>> part3([0], 8)
+    3
+    >>> part3([1], 1)
+    1
+    >>> part3([3,4,3,1,2], 2)
+    6
+    >>> part3([3,4,3,1,2], 4)
+    9
+    >>> part3([3,4,3,1,2], 5)
+    10
+    >>> part3([3,4,3,1,2], 10)
+    12
+    >>> part3([3,4,3,1,2], 12)
+    17
+    >>> part3([3,4,3,1,2], 18)
+    26
+    >>> part3([3,4,3,1,2],80)
+    5934
+    """
+
+    new = {}
+    for fish in data:
+        count = new.get(fish, 0)
+        new[fish] = count + 1
+
+    for day in range(days):
+        fishes = dict(new)
+        new = {}
+        for fish in fishes:
+            count = fishes[fish]
+            if fish == 0:
+                fish = 6
+                a = new.get(8, 0)
+                new[8] = a + count
+            else:
+                fish = fish - 1
+
+            a = new.get(fish, 0)
+            new[fish] = a + count
+
+    return sum(new.values())
+
+
 if __name__ == '__main__':
     with open('input') as f:
         line = f.readline().strip()
@@ -92,4 +147,4 @@ if __name__ == '__main__':
 
     i = list(map(int, line.split(',')))
     print('Part 1: ', part1(i, 80))
-    print('Part 2: ', part2(i, 256))
+    print('Part 2: ', part3(i, 256))
