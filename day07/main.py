f5f9aa4dfe04a2bd5ebac9c9ec1051c4130935e1
Rait Kapp <3442567+raigu@users.noreply.github.com>
2021-12-07 07:24:43 +0200

    Day07 initial solution

--- day07/main.py
+++ day07/main.py
@@ -1,25 +1,44 @@
+import sys
 
-def part1(data):
-    answer = 0
 
-    for line in data:
-        answer += 1
-
-    return answer
+def consumption(b, e):
+    """
+    >>> consumption(16,5)
+    66
+    >>> consumption(14, 5)
+    45
+    """
+    r = 0
+    for i in range(abs(e - b)):
+        r += i + 1
+    return r
 
 
 def part2(data):
-    answer = 0
+    min = sys.maxsize
+    for e in range(len(data)):
+        fuel = 0
+        for b in range(len(data)):
+            fuel += consumption(data[b], e)
+        if fuel < min:
+            min = fuel
+    return min
 
-    for line in data:
-        answer += 1
 
-    return answer
+def part1(data):
+    min = sys.maxsize
+    for e in range(len(data)):
+        fuel = 0
+        for b in range(len(data)):
+            fuel += abs(data[e] - data[b])
+        if fuel < min:
+            min = fuel
+    return min
 
 
 if __name__ == '__main__':
-    with open('input1') as f:
-        lines = [line.strip() for line in f.readlines()]
+    with open('input') as f:
+        lines = [int(line.strip()) for line in f.readline().split(',')]
 
     print(f'Part1: {part1(lines)}')
     print(f'Part2: {part2(lines)}')
