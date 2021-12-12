a791b4d5cfd8fa875f56b6e2a07fce1268000629
Rait Kapp <3442567+raigu@users.noreply.github.com>
2021-12-12 07:23:56 +0200

    WIP

--- day12/main.py
+++ day12/main.py
@@ -1,11 +1,34 @@
 
+def to_end(start, points, visited) -> list:
+    ret = [[]]
+
+    if start in points:
+        neighbours = points[start]
+    else:
+        neighbours
+    for n in points[start]:
+        if (start, n) not in visited:  # avoid loops
+
+            if n[0] >= 'a': # big cave
+                done = False
+                for (s,e) in visited:
+                    if s == n[0] or e == n[0]:
+                        done = True
+                        break
+                if done:
+                    continue # skip looking to subpathes
+
+            next_visited = set(visited)
+            next_visited.add((start, n))
+            ret.append([start] + to_end(n, points, next_visited))
+
+    return ret
+
 def part1(data):
-    answer = 0
 
-    for line in data:
-        answer += 1
+    pathes = to_end('start', data, set())
 
-    return answer
+    return len(pathes)
 
 
 def part2(data):
@@ -18,9 +41,17 @@ def part2(data):
 
 
 if __name__ == '__main__':
+    points = []
     with open('input1') as f:
-        lines = [line.strip() for line in f.readlines()]
+        for line in [line.strip() for line in f.readlines()]:
+            (b, e) = line.strip().split('-')
+            points.append(ends)
+            ends = points.get(b, [])
+            ends.append(e)
+            points[b] = ends
 
-    print(f'Part1: {part1(lines)}')
-    print(f'Part2: {part2(lines)}')
+    print(points)
+
+    print(f'Part1: {part1(points)}')
+    print(f'Part2: {part2(points)}')
 
