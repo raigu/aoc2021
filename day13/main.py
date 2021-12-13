aecc0e6bd1ee4fad6d4251ffc5c097e3e500cb5a
Rait Kapp <3442567+raigu@users.noreply.github.com>
2021-12-13 07:37:54 +0200

    Day 13 solution

--- day13/main.py
+++ day13/main.py
@@ -1,26 +1,64 @@
+def fold(dots, instruction):
+    """
+    >>> fold({(2,2)}, ('x', 1))
+    {(0, 2)}
+    >>> fold({(0,2)}, ('y', 1))
+    {(0, 0)}
+    """
+    next = set()
+    if instruction[0] == 'x':
+        for x, y in dots:
+            if x > instruction[1]:
+                next.add((instruction[1] - (x - instruction[1]), y))
+            else:
+                next.add((x, y))
+    else:
+        for x, y in dots:
+            if y > instruction[1]:
+                next.add((x, instruction[1] - (y - instruction[1])))
+            else:
+                next.add((x, y))
 
-def part1(data):
-    answer = 0
-
-    for line in data:
-        answer += 1
-
-    return answer
+    return next
 
 
-def part2(data):
-    answer = 0
+def output(points):
+    max_x = max(x for x, y in points)
+    max_y = max(y for x, y in points)
 
-    for line in data:
-        answer += 1
-
-    return answer
+    for y in range(max_y+1):
+        row = []
+        for x in range(max_x+1):
+            if (x, y) in points:
+                row.append('#')
+            else:
+                row.append('.')
+        print(''.join(row))
 
 
 if __name__ == '__main__':
-    with open('input1') as f:
+    with open('input') as f:
         lines = [line.strip() for line in f.readlines()]
+        instructions = []
+        dots = set()
+        for line in lines:
+            if line == '':
+                continue
 
-    print(f'Part1: {part1(lines)}')
-    print(f'Part2: {part2(lines)}')
+            if line.startswith('fold along'):
+                (d, pos) = line[11:].split('=')
+                instructions.append((d, int(pos)))
+            else:
+                x, y = line.split(',')
+                dots.add((int(x), int(y)))
 
+    print(dots)
+
+    a = len(fold(dots, instructions[0]))
+
+    print(f'Part1: {a}')
+
+    for i in range(len(instructions)):
+        dots = fold(dots, instructions[i])
+
+    output(dots)
