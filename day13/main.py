c72140544092cf631be38465a5a0a5e9e4f20a3f
Rait Kapp <3442567+raigu@users.noreply.github.com>
2021-12-13 07:40:01 +0200

    Refine

--- day13/main.py
+++ day13/main.py
@@ -6,18 +6,13 @@ def fold(dots, instruction):
     {(0, 0)}
     """
     next = set()
-    if instruction[0] == 'x':
-        for x, y in dots:
-            if x > instruction[1]:
-                next.add((instruction[1] - (x - instruction[1]), y))
-            else:
-                next.add((x, y))
-    else:
-        for x, y in dots:
-            if y > instruction[1]:
-                next.add((x, instruction[1] - (y - instruction[1])))
-            else:
-                next.add((x, y))
+    for x, y in dots:
+        if instruction[0] == 'x' and x > instruction[1]:
+            next.add((instruction[1] - (x - instruction[1]), y))
+        elif instruction[0] == 'y' and y > instruction[1]:
+            next.add((x, instruction[1] - (y - instruction[1])))
+        else:
+            next.add((x, y))
 
     return next
 
@@ -52,13 +47,11 @@ if __name__ == '__main__':
                 x, y = line.split(',')
                 dots.add((int(x), int(y)))
 
-    print(dots)
-
     a = len(fold(dots, instructions[0]))
-
     print(f'Part1: {a}')
 
     for i in range(len(instructions)):
         dots = fold(dots, instructions[i])
 
+    print('Part2:')
     output(dots)
