5ae109de0f3772d05e1f6be53d438bf0f43e2529
Rait Kapp <3442567+raigu@users.noreply.github.com>
2021-12-17 09:20:35 +0200

    Cleanup

--- day17/main.py
+++ day17/main.py
@@ -1,24 +1,3 @@
-from math import sqrt
-
-
-def part1(data):
-    answer = 0
-
-    for line in data:
-        answer += 1
-
-    return answer
-
-
-def part2(data):
-    answer = 0
-
-    for line in data:
-        answer += 1
-
-    return answer
-
-
 def next(p, v):
     x, y = p
     vx, vy = v
@@ -56,14 +35,13 @@ if __name__ == '__main__':
     xt = (88, 125)
     yt = (-157, -103)
 
-    #xt = (20, 30)
-    #yt = (-10, -5)
+    # xt = (20, 30)
+    # yt = (-10, -5)
 
     max_y = -9999
-    x_range = (0, max(xt)+1)
-    y_range = (min(yt)-2, 3000)
+    x_range = (0, max(xt) + 1)
+    y_range = (min(yt) - 2, 3000)
     sui = set()
-    max_steps = sqrt(2*max(xt))
     for x in range(x_range[0], x_range[1]):
         for y in range(y_range[0], y_range[1]):
             p = (0, 0)
@@ -76,19 +54,9 @@ if __name__ == '__main__':
                     current_max_y = p[1]
 
             if reached(p, xt, yt):
-                sui.add((x,y))
+                sui.add((x, y))
                 if max_y < current_max_y:
                     max_y = current_max_y
-                print(f'({x},{y})', ' -> ', p, v, ':', current_max_y, max_y)
 
-    # 12246
-    print('Part1', max_y)
-    # 165 not right
-    # 1438 is not right
-    # 1475 not right
-    # 399 not right
-    # 3473 not right
-    # 3528 <- canditate
-    # 1172 ???
-    #print(sui)
-    print('Part2', len(sui))
+    print('Part1:', max_y)
+    print('Part2:', len(sui))
