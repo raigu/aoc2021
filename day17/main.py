a0c34146a8eb378e3a9042c33e134afde22bb01d
Rait Kapp <3442567+raigu@users.noreply.github.com>
2021-12-17 09:17:55 +0200

    Day 17 Part2 solution

--- day17/main.py
+++ day17/main.py
@@ -1,3 +1,6 @@
+from math import sqrt
+
+
 def part1(data):
     answer = 0
 
@@ -57,23 +60,35 @@ if __name__ == '__main__':
     #yt = (-10, -5)
 
     max_y = -9999
-    x_range = (0, 125)
-    y_range = (0, 10000)
+    x_range = (0, max(xt)+1)
+    y_range = (min(yt)-2, 3000)
+    sui = set()
+    max_steps = sqrt(2*max(xt))
     for x in range(x_range[0], x_range[1]):
         for y in range(y_range[0], y_range[1]):
             p = (0, 0)
             v = (x, y)
             current_max_y = -9999
+            steps = 0
             while not reached(p, xt, yt) and not passes(p, v, xt, yt):
                 p, v = next(p, v)
                 if current_max_y < p[1]:
                     current_max_y = p[1]
 
             if reached(p, xt, yt):
+                sui.add((x,y))
                 if max_y < current_max_y:
                     max_y = current_max_y
                 print(f'({x},{y})', ' -> ', p, v, ':', current_max_y, max_y)
 
     # 12246
     print('Part1', max_y)
-    print('end')
+    # 165 not right
+    # 1438 is not right
+    # 1475 not right
+    # 399 not right
+    # 3473 not right
+    # 3528 <- canditate
+    # 1172 ???
+    #print(sui)
+    print('Part2', len(sui))
