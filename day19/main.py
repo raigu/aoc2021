1655e3e91602f115d59781f8c3548ab64265759b
Rait Kapp <3442567+raigu@users.noreply.github.com>
2021-12-19 13:32:06 +0200

    Day 19 WIP

--- day19/main.py
+++ day19/main.py
@@ -37,13 +37,6 @@ def identity_matrix(n):
     return ret
 
 
-def multiply(matrix, vector):
-    """
-    >>> multiply([[1,2], []])
-    [[2,-1]]
-    """
-
-
 class Vector:
     """
     >>> Vector(1,2,3) + Vector(5,2,2)
@@ -83,7 +76,19 @@ class Vector:
 
         return True
 
+    def transform(self, direction: list) -> 'Vector':
+        """
+        >>> Vector(1, 2).transform([[0, 1], [1, 0]])
+        Vector(2,1)
+        >>> Vector(1,2,3).transform([[1,0,0],[0,1,0],[0,0,1]])
+        Vector(1,2,3)
+        """
 
+        ret = []
+        for row in direction:
+            ret.append(sum(d * c for d, c in zip(row, self.coordinates)))
+
+        return Vector(*ret)
 
     def all_directions(self):
         """
@@ -98,7 +103,7 @@ class Vector:
             for coordinate in permutations(self.coordinates, len(self.coordinates)):
                 coordinate = list(coordinate)
                 for i, c in enumerate(coordinate):
-                    coordinate[i] = c*shift[i]
+                    coordinate[i] = c * shift[i]
                 ret.append(Vector(*coordinate))
         return ret
 
@@ -109,9 +114,42 @@ class Vector:
         coordinates = ','.join([str(c) for c in self.coordinates])
         return f'Vector({coordinates})'
 
+
+def all_directions(n) -> list:
+    rows = []
+    for row in list(product([-1, 0, 1], repeat=n)):
+        if len([x for x in row if x == 0]) == n - 1:
+            rows.append(row)
+
+    ret = []
+    for direction in list(product(rows, repeat=n)):
+        i = 0
+        suits = True
+        while i < n and suits:
+            col = [direction[j][i] for j in range(n)]
+            if len([x for x in col if x == 0]) != n - 1:
+                suits = False
+            else:
+                i += 1
+        if suits:
+            ret.append(direction)
+
+    return ret
+
+
 if __name__ == '__main__':
     scanners = []
-    with open('input2') as f:
+
+    input = 2
+
+    if input == 1:
+        minimum_match_requirement = 3
+        directions = all_directions(2)
+    else:
+        minimum_match_requirement = 12
+        directions = all_directions(3)
+
+    with open(f'input{input}') as f:
         lines = [line.strip() for line in f.readlines()]
         j = -1
         for line in lines:
@@ -121,29 +159,30 @@ if __name__ == '__main__':
             elif line != '':
                 scanners[j].add(Vector(*list(map(int, line.split(',')))))
 
-    directions = [
-        [
-            [1, 0, 0],
-            [0, 1, 0],
-            [0, 0, 1]
-        ]
-    ]
 
-    beacons = set()
-    minimum_match_requirement = 12
-    for sui0 in scanners[0]:
-        for sui1 in scanners[1]:
-            for direction in directions:
-                matched = set()
-                alternate = sui1.transform(direction)
-                alternate = alternate - sui0
 
-                matched = sui0.intersection(sui1)
-                if len(matched) >= minimum_match_requirement:
-                    beacons |= matched
-                    break
+    beacons = scanners[0] # first scanner beacons we already know
 
+    for i in range(len(scanners)):
+        j = i + 1
+        while j < len(scanners):
+            print(i, j)
+            for sui0 in scanners[i]:
+                for sui1 in scanners[j]:
+                    s2 = sui0 - sui1
+                    for direction in directions:
+                        transformed = set()
+                        for beacon in scanners[j]:
+                            beacon = beacon.transform(direction)
+                            transformed.add(s2 + beacon)
+
+                        matched = transformed.intersection(scanners[0])
+                        if len(matched) >= minimum_match_requirement:
+                            beacons |= matched
+                            break
+            j += 1
     print(beacons)
+    print(len(beacons))
 
     print('Day 19')
     print(f'Part1: {part1(lines)}')
