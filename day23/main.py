3c4679a8e6f0624daddcefdc774b05508b9a5583
Rait Kapp <3442567+raigu@users.noreply.github.com>
2021-12-23 11:37:04 +0200

    Add Day 23 WIP

--- day23/main.py
+++ day23/main.py
@@ -68,7 +68,7 @@ class Burrow:
     @classmethod
     def from_file(cls, filename):
         burrow = Burrow()
-        with open('input1') as f:
+        with open(filename) as f:
             i = 0
             for line in f.readlines():
                 for j, c in enumerate(line):
@@ -94,12 +94,15 @@ def step_energy_of_aphipod(a: str) -> int:
 
 def is_home(space, y1, x1) -> bool:
     a = space[y1][x1]
-    if y1 > 1:  # at hole
-        i = 2 + (ord(a) - ord('A')) * 2
-        if i == x1:
-            return True
+    if not is_above_home(a, x1):
+        return False
 
-    return False
+    while space[y1 + 1][x1] != '#':
+        y1 += 1
+        if space[y1][x1] != a:
+            return False
+
+    return True
 
 
 def all_possible_paths(space, y1, x1) -> list[list[list]]:
@@ -150,43 +153,88 @@ def print_space(space):
     for layer in space:
         print(''.join(layer))
 
+
 def space_hash(space):
     return hash(''.join(''.join(layer) for layer in space))
 
-covered = set()
 
-def min_energy(space) -> int:
-    ret = 0
+def is_above_home(c, x):
+    """
+    >>> is_above_home('A', 3)
+    True
+    >>> is_above_home('B', 5)
+    True
+    >>> is_above_home('A', 2)
+    False
+    """
+    return x == 3 + (ord(c) - ord('A')) * 2
+
+
+def min_energy(space, covered) -> int:
+    covered = copy.deepcopy(covered)
+    print_space(space)
+    ret = sys.maxsize
+
+    down = [1, 0]
+    up = [-1, 0]
+    left = [0, -1]
+    right = [0, 1]
 
     i = 1
     while i < len(space) - 1:
         j = 1
         while j < len(space[0]) - 1:
-            if is_amphipod(space[i][j]):
-                y = i
-                x = j
-                for path in all_possible_paths(space, i, j):
-                    step_energy = step_energy_of_aphipod(space[i][j])
-                    energy = 0
-                    for step in path:
-                        y += step[0]
-                        x += step[1]
-                        energy += step_energy
+            if is_amphipod(space[i][j]) and not is_home(space, i, j):
+                directions = [up, down, left, right]
+                for direction in directions:
+                    y = i + direction[0]
+                    x = j + direction[1]
 
-                    new = copy.deepcopy(space)
-                    new[y][x] = space[i][j]
-                    new[i][j] = ' '
+                    if space[y][x] == ' ':
+                        if direction == down:
+                            # Amphipods will never move from the hallway into a room unless that room is their destination room ...
+                            if not is_above_home(space[i][j], x):
+                                continue
 
-                    h = space_hash(new)
-                    if h not in covered:
-                        covered.add(h)
-                        print_space(new)
+                            # move as down as possible
+                            while space[y + 1][x] == ' ':
+                                y += 1
 
-                        m = min_energy(new)
-                        if m + energy < ret:
-                            ret = m + energy
+                            # ... and that room contains no amphipods which do not also have that room as their own destination.
+                            if not (space[y + 1][x] == '#' or space[y + 1][x] == space[i][j]):
+                                continue
+
+                        new = copy.deepcopy(space)
+                        new[y][x] = space[i][j]
+                        new[i][j] = ' '
+
+                        h = space_hash(new)
+                        if h not in covered:
+                            covered.add(h)
+
+                            m = min_energy(new, covered)
+
+                            if ret > m + step_energy_of_aphipod(space[i][j]):
+                                ret = m + step_energy_of_aphipod(space[i][j])
+
             j += 1
         i += 1
+
+    if ret == sys.maxsize:  # no moves, is it final?
+        suits = True
+        i = 0
+        while i < 4 and suits:
+            c = ord('A') + i
+            x = 3 + ((c - ord('A')) * 2)
+            y = 2
+            while y < len(space) - 1 and suits:
+                if ord(space[y][x]) != c:
+                    suits = False
+                else:
+                    y += 1
+            i += 1
+        if suits:
+            return 0  # yee! final!
 
     return ret
 
@@ -194,9 +242,9 @@ def min_energy(space) -> int:
 if __name__ == '__main__':
     print('Day 23')
 
-    burrow = Burrow.from_file('input1')
+    burrow = Burrow.from_file('input2')
 
-    part1 = min_energy(burrow.space)
+    part1 = min_energy(burrow.space, set())
 
     print(f'Part1: {part1}')
 
