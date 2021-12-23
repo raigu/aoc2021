14d194f5b5bf51e6c0912d1f91861f91c4888e4e
Rait Kapp <3442567+raigu@users.noreply.github.com>
2021-12-23 15:24:14 +0200

    Day 23 solution

--- day23/main.py
+++ day23/main.py
@@ -8,12 +8,14 @@ def print_space(space):
     for layer in space:
         print(''.join(layer))
 
+
 def space_hash(space):
     return hash(''.join(''.join(layer) for layer in space))
 
 
 def is_amphipod(c: str) -> bool:
     return c in ['A', 'B', 'C', 'D']
+
 
 def is_above_home(c, x):
     """
@@ -26,9 +28,10 @@ def is_above_home(c, x):
     """
     return x == 3 + (ord(c) - ord('A')) * 2
 
+
 def is_home(space, y1, x1) -> bool:
     if y1 < 2:
-        return False # in a hallway
+        return False  # in a hallway
 
     a = space[y1][x1]
     if not is_above_home(a, x1):
@@ -41,6 +44,7 @@ def is_home(space, y1, x1) -> bool:
 
     return True
 
+
 def step_energy_of_aphipod(a: str) -> int:
     map = {
         'A': 1,
@@ -50,6 +54,7 @@ def step_energy_of_aphipod(a: str) -> int:
     }
     return map[a]
 
+
 def neighbours(space):
     ret = []
 
@@ -58,21 +63,20 @@ def neighbours(space):
     i = 1
     while i < len(space) - 1:
         j = 1
-        while j < len(space[0])-1:
+        while j < len(space[0]) - 1:
             if is_amphipod(space[i][j]) and not is_home(space, i, j):
                 c = space[i][j]
-                if i == 1: # in the hall
-                    amphipods_home.put((-step_energy_of_aphipod(c), (c,i,j)))
-                else: # in the hole
-                    if space[i-1][j] == '.': # can move up?
+                if i == 1:  # in the hall
+                    amphipods_home.put((-step_energy_of_aphipod(c), (c, i, j)))
+                else:  # in the hole
+                    if space[i - 1][j] == '.':  # can move up?
                         amphipods_out.put((step_energy_of_aphipod(c), (c, i, j)))
             j += 1
         i += 1
 
-
     # priority is to get home
     while not amphipods_home.empty():
-        (e, (c,i,j)) = amphipods_home.get()
+        (e, (c, i, j)) = amphipods_home.get()
 
         # is in the hallway. Let's see if it can move back to home
         home_index = 3 + (ord(c) - ord('A')) * 2
@@ -93,8 +97,16 @@ def neighbours(space):
                 y += 1
                 energy += step_energy_of_aphipod(c)
 
+            # make sure there are no other aphipd
+            u = 1
+            while space[y + u][x] != '#' and not blocked:
+                if space[y + u][x] != c:
+                    blocked = True
+                else:
+                    u += 1
+
             # ... and that room contains no amphipods which do not also have that room as their own destination.
-            if space[y + 1][x] == '#' or space[y + 1][x] == c:
+            if not blocked:
                 new = copy.deepcopy(space)
                 new[y][x] = c
                 new[i][j] = '.'
@@ -111,6 +123,51 @@ def neighbours(space):
             y -= 1
             energy += step_energy_of_aphipod(c)
 
+        # first try to get to own hole.
+        x2 = x
+        y2 = y
+        energy2 = energy
+        # ----
+        # is in the hallway. Let's see if it can move back to home
+        home_index = 3 + (ord(c) - ord('A')) * 2
+        direction = -1 if home_index < x else 1
+        blocked = home_index == x  # lets interrupt also if it crawled out from same hole ;)
+        while home_index != x and not blocked:
+            x += direction
+            energy += step_energy_of_aphipod(c)
+            if space[y][x] != '.':
+                blocked = True
+
+        if not blocked:  # at the top of hole.
+            # move as down as possible
+            while space[y + 1][x] == '.':
+                y += 1
+                energy += step_energy_of_aphipod(c)
+
+            # make sure there are no other aphipd
+            u = 1
+            while space[y + u][x] != '#' and not blocked:
+                if space[y + u][x] != c:
+                    blocked = True
+                else:
+                    u += 1
+
+            # ... and that room contains no amphipods which do not also have that room as their own destination.
+            if not blocked:
+                new = copy.deepcopy(space)
+                new[y][x] = c
+                new[i][j] = '.'
+
+                ret.append([new, energy])
+
+                continue  # moving one hole to another is best option, do not search other options.
+        # ---
+
+        # restore what was before checking if can go to hole
+        y = y2
+        x = x2
+        energy = energy2
+
         # when reached out, move as far left as you can and produce possible outcomes
         x2 = x
         left_energy = energy
@@ -119,7 +176,7 @@ def neighbours(space):
             left_energy += step_energy_of_aphipod(c)
 
             # do not stop on top of hole:
-            if x2 not in [3,5,7,9]:
+            if x2 not in [3, 5, 7, 9]:
                 new = copy.deepcopy(space)
                 new[y][x2] = space[i][j]
                 new[i][j] = '.'
@@ -143,28 +200,33 @@ def neighbours(space):
 
     return ret
 
-def solution(space, end_hash) -> dict:
+
+def dijkstra(space, end_hash):
+    mapping = {}
+    parents = {}
     distances = defaultdict(lambda: sys.maxsize)
     s = space_hash(space)  # start
-    distances[end_hash] = 100000 # lets look for solution below this number
+    distances[end_hash] = 100000  # lets look for solution below this number
     distances[s] = 0  # distance from start to start is known
     queue = PriorityQueue()
     queue.put((0, space))
     while not queue.empty():
         d, v = queue.get()
-        print("\nNEW");        print_space(v)
+        parent_hash = space_hash(v)
         for n, edge in neighbours(v):
-            print("----");           print_space(n);
             i = space_hash(n)
+
+            if i not in mapping:
+                mapping[i] = n
+
             if distances[i] > d + edge:
                 distances[i] = d + edge
-                if i == end_hash:
-                    print('Found: ', distances[i])
-                #print(d+edge)
+                parents[i] = parent_hash
                 if distances[i] < distances[end_hash]:
                     queue.put((distances[i], n))
 
-    return distances
+    return distances, parents, mapping
+
 
 def load_space(filename):
     space = []
@@ -177,19 +239,32 @@ def load_space(filename):
             space.append(row)
     return space
 
+
 if __name__ == '__main__':
     print('Day 23')
 
-    space = load_space('sample_part2')
-
-    print("Initial space:")
-    print_space(space)
-
-
-    final = load_space('final_part2')
+    space = load_space('input_part1')
+    final = load_space('final_part1')
     h = space_hash(final)
-
-    # 46148 <- too low
-    distances = solution(space, h)
+    distances, parents, mapping = dijkstra(space, h)
     print('Part1: ', distances[h])
 
+    space = load_space('input_part2')
+    final = load_space('final_part2')
+    h = space_hash(final)
+    distances, parents, mapping = dijkstra(space, h)
+    print('Part2: ', distances[h])
+    s = space_hash(space)
+
+    print("Solution (backward)")
+    print("  (commented out in code)")
+    '''
+    i = h
+    while i != s:
+        print("")
+        print_space(mapping[i])
+        i = parents[i]
+
+    print("")
+    print_space(space)
+    '''
