22171f38af9ba73fab94ad51956fffa8dfd41b20
Rait Kapp <3442567+raigu@users.noreply.github.com>
2021-12-25 11:01:56 +0200

    Day 25 solution

--- day25/main.py
+++ day25/main.py
@@ -1,26 +1,70 @@
 
-def part1(data):
-    answer = 0
-
-    for line in data:
-        answer += 1
-
-    return answer
-
-
-def part2(data):
-    answer = 0
-
-    for line in data:
-        answer += 1
-
-    return answer
+def print_cucumbers(cucumbers, rows, cols):
+    for y in range(rows):
+        line = []
+        for x in range(cols):
+            if (y, x) in cucumbers:
+                c = cucumbers[(y, x)]
+            else:
+                c = '.'
+            line.append(c)
+        print(''.join(line))
 
 
 if __name__ == '__main__':
-    with open('input1') as f:
+    floor = []
+    with open('input') as f:
         lines = [line.strip() for line in f.readlines()]
 
-    print(f'Part1: {part1(lines)}')
-    print(f'Part2: {part2(lines)}')
+    cucumbers = {}
+    rows = 0
+    cols = 0
+    for line in lines:
+        rows += 1
+        cols = 0
+        for cols, cucumber in enumerate(line):
+            if cucumber in ['>', 'v']:
+                cucumbers[(rows - 1, cols)] = cucumber
+    cols += 1
 
+    any = True
+    steps = 0
+    while any:
+        # print(steps)
+        # print_cucumbers(cucumbers, rows, cols)
+        next = {}
+        any = False
+
+        # east
+        for p, cucumber in cucumbers.items():
+            if cucumber != '.':
+                if cucumber == '>':
+                    y = p[0]
+                    x = (p[1] + 1) % cols
+                    if (y, x) not in cucumbers:
+                        any = True
+                        next[(y, x)] = cucumber
+                        continue
+                next[p] = cucumber
+
+        cucumbers = next
+
+        # south
+        next1 = {}
+        for p, cucumber in next.items():
+            if cucumber != '.':
+                if cucumber == 'v':
+                    y = (p[0] + 1) % rows
+                    x = p[1]
+                    if (y, x) not in cucumbers:
+                        any = True
+                        next1[(y, x)] = cucumber
+                        continue
+                next1[p] = cucumber
+
+        cucumbers = next1
+        steps += 1
+
+    print('Day 25')
+    print(f'Part1: {steps}')
+    print(f'Part2: RTFI')
